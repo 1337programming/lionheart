eb99bdd01365d8d3a9c93b51473bc09fdb330e77
Tom Manion <thomaswmanion@gmail.com>
2016-02-26 16:06:31 -0600

    Adding hiding and showing the navbar and footer on login/logout

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Http, Headers} from 'angular2/http';
 import {NgIf} from 'angular2/common';
 import {Router} from 'angular2/router';
@@ -12,11 +12,19 @@ let style = require('!!raw!sass!./footer.scss');
 	styles: [style]
 })
 
-export class Footer {
-	constructor(private http: Http, private router: Router, private userService: UserService) {
+export class Footer implements OnInit {
+	loggedIn: boolean;
+	constructor(private http: Http, private router: Router, private userService: UserService) {}
+
+	ngOnInit() {
+		this.userService.loggedInSubject.subscribe((loggedInStatus) => {
+			this.loggedIn = loggedInStatus;
+		});		
 	}
+
 	logout() {
 		localStorage.removeItem('jwt');
+		this.userService.updateLoggedInStatus();
 		this.router.navigate(['/Login']);
 	}
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,14 +1,16 @@
 import {Injectable} from 'angular2/core';
-
+import {Subject, BehaviorSubject} from 'rxjs';
 @Injectable()
 export class UserService {
 	jwtToken: string;
+	public loggedInSubject: Subject<boolean> = new BehaviorSubject<boolean>(false);
 	constructor() {
 		this.updateLoggedInStatus();
 	}
 
 	updateLoggedInStatus(): void {
 		this.jwtToken = localStorage.getItem('jwt');
+		this.loggedInSubject.next(this.isLoggedIn);
 	}
 
 	get isLoggedIn(): boolean {
